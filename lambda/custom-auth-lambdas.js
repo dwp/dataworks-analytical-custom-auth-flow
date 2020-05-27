f5c0a8d9718eb1df648fc4d8b7cef552e433e6ef
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2020-05-27 10:05:58 +0100

    Refactor and replace type-converting to strict equality operators (#8)

--- lambda/custom-auth-lambdas.js
+++ lambda/custom-auth-lambdas.js
@@ -3,33 +3,37 @@ const crypto = require("crypto-secure-random-digit");
 const sendSMS = require('./aws/sendSMS.js').sendSMS
 const getUserDetails = require('./aws/getUserDetails').getUserDetails;
 
+
 module.exports.createAuthChallenge = async (event = {}, context) => {
-    var mobileNumber = ()=> {
-        if(event.request.userAttributes.phone_number == null||event.request.userAttributes.phone_number == undefined){
-            throw new Error("No phone number provided")
-        }
-        else return event.request.userAttributes.phone_number;
-    }
+
+    const userPhoneNumber = event.request.userAttributes.phone_number;
+    if (!userPhoneNumber) throw new Error("No phone number provided");
+
     const oneTimeAuthCode = crypto.randomDigits(6).join('');
-    await sendSMS(mobileNumber(), oneTimeAuthCode, event.userName)
-    event.response.privateChallengeParameters = { "otp" : oneTimeAuthCode };
-    
+    await sendSMS(userPhoneNumber, oneTimeAuthCode, event.userName);
+
+    event.response.privateChallengeParameters = {otp: oneTimeAuthCode};
     return event
 }
 
 module.exports.defineAuthChallenge = async (event, context) => {
     let userDetails = await getUserDetails(event.userPoolId, event.userName)
-    let existingMFA = (userDetails.PreferredMfaSetting == "SOFTWARE_TOKEN_MFA")
+    let existingMFA = (userDetails.PreferredMfaSetting === "SOFTWARE_TOKEN_MFA")
 
-    if (event.request.session.length == 1 && event.request.session[0].challengeName == 'SRP_A') {
+    const {session} = event.request;
+
+    /* Verify password*/
+    if (session.length === 1 && session[0].challengeName === 'SRP_A') {
         event.response.issueTokens = false;
         event.response.failAuthentication = false;
         event.response.challengeName = 'PASSWORD_VERIFIER';
-    } else if (!existingMFA && event.request.session.length == 2 && event.request.session[1].challengeName == 'PASSWORD_VERIFIER' && event.request.session[1].challengeResult == true) {
+        /* Issue custom challenge if user entered correct password but doesn't have MFA setup */
+    } else if (!existingMFA && session.length === 2 && session[1].challengeName === 'PASSWORD_VERIFIER' && session[1].challengeResult === true) {
         event.response.issueTokens = false;
         event.response.failAuthentication = false;
         event.response.challengeName = 'CUSTOM_CHALLENGE';
-    } else if ((existingMFA && event.request.session[1].challengeName == 'PASSWORD_VERIFIER' && event.request.session[1].challengeResult == true) || (event.request.session.length == 3 && event.request.session[2].challengeName == 'CUSTOM_CHALLENGE' && event.request.session[2].challengeResult == true)) {
+    } else if ((existingMFA && session[1].challengeName === 'PASSWORD_VERIFIER' && session[1].challengeResult === true) // Issue tokens if user has mfa and correct password
+        || (session.length === 3 && session[2].challengeName === 'CUSTOM_CHALLENGE' && session[2].challengeResult === true)) { // Issue tokens if user answered correctly to custom challenge
         event.response.issueTokens = true;
         event.response.failAuthentication = false;
     } else {
@@ -40,13 +44,8 @@ module.exports.defineAuthChallenge = async (event, context) => {
     return await event;
 }
 
-module.exports.verifyAuthChallenge = async(event) => {
+module.exports.verifyAuthChallenge = async (event) => {
     const oneTimeAuthCode = event.request.privateChallengeParameters.otp;
-    if(event.request.challengeAnswer === oneTimeAuthCode){
-        event.response.answerCorrect = true;
-    }
-    else {
-        event.response.answerCorrect = false;
-    }
+    event.response.answerCorrect = event.request.challengeAnswer === oneTimeAuthCode;
     return event
 }
