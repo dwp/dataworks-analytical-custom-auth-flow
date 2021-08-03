47472aa2b0f04db34c1ffdeeb43cbd3282da3a91
Ian Utley <ian.utley@accenture.com>
2021-08-03 15:39:07 +0100

    Unknown user shouldnt error

--- lambda/aws/__mocks__/getUserDetails.js
+++ lambda/aws/__mocks__/getUserDetails.js
@@ -2,5 +2,8 @@ module.exports.getUserDetails = async function (userPoolId,userName){
     if(userName == "noMfa"){
         return {PreferredMfaSetting: null}
     }
+    if(userName == "unknown") {
+        throw 'User unknown'
+    }
     return {PreferredMfaSetting: "SOFTWARE_TOKEN_MFA"}
 }
